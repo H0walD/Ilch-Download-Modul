79475af3f3fda9479eef8fcb035e0faf2b8bf472
Balthazar3k <angelo.b3k@gmail.com>
2014-10-18 14:40:05 +0200

    Update: Installfile

--- upload/include/includes/module/download.php
+++ upload/include/includes/module/download.php
@@ -3,22 +3,33 @@ defined('main') or die('Sorry, so aber nicht!');
 
 class download {
 
-    public $author = '2014 by <a href="http://balthazar3k.howald-design.ch/">angelo.b3k</a> & <a href="http://howald-design.ch/">howaldi</a>';
+    public $author = '2014 by <a href="http://balthazar3k.howald-design.ch/">angelo.b3k</a> & <a href="http://howald-design.ch/">Howaldi</a>';
     public $description = 'Das Download Script erweitert das Standart Download Script von ilch!';
 
     public function folders(){
-        return array(
+        $folders = array(
             'include/images/download/',
             'include/images/downcats/'
         );
+
+        foreach( $folders as $folder ){
+            @mkdir($folder);
+        }
+
+        return $folders;
     }
 
     public function install(Install $install) {
-        return db_query("
-            ALTER TABLE `prefix_downcats` ADD `img` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
-            ALTER TABLE `prefix_downloads` ADD `demo` VARCHAR( 255 ) CHARACTER SET latin1 COLLATE latin1_german2_ci NOT NULL AFTER `ssurl`;
-            ALTER TABLE `prefix_downloads` ADD `drecht` TINYINT(4) NOT NULL DEFAULT '0';
-        ");
+        $status = array();
+        $status[] = db_query("ALTER TABLE `prefix_downcats` ADD `img` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;");
+        $status[] = db_query("ALTER TABLE `prefix_downloads` ADD `demo` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `ssurl`;"); 
+        $status[] = db_query("ALTER TABLE `prefix_downloads` ADD `drecht` TINYINT(4) NOT NULL DEFAULT '0';");
+
+        return !in_array(false, $status);
+    }
+
+    public function deinstall() {
+        
     }
 }
 ?>
